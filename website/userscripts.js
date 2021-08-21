ca4e1250cdb6651c4aa2c2e2732c3f13a138b227
gissehel <public-devgit-common@gissehel.org>
2021-08-21 17:58:54 +0200

    DOC : Add some basic style to the homepage

--- website/userscripts.js
+++ website/userscripts.js
@@ -114,6 +114,10 @@ const getProperty = (userscript, name, defaultValue) => {
  * @param {{[path: string]: {[property: string]:string}[]}} userscripts 
  */
 const createPage = (userscripts) => {
+    addStyle('table { width: 100% }')
+    addStyle('th,td { border: 1px solid; padding: 0.6em; }')
+    addStyle('th,td,p,div,span,h1,h2,h3,body { font-family: "Calibri","sans-serif"; }')
+
     createElement('title', { parent: document.head, text: 'Userscripts' });
     createElement('h1', { parent: document.body, text: 'Userscripts' });
     createElement('table', {
