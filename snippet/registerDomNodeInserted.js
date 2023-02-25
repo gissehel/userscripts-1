21943cbcea1c7b774c3fc16088c2ed27b9059809
gissehel <public-devgit-common@gissehel.org>
2023-02-25 16:19:01 +0100

    nightcafe-add-hot-new-buttons : Version 1.0.0

---
+++ snippet/registerDomNodeInserted.js
@@ -0,0 +1,23 @@
+/**
+ * Add a DOMNodeInserted on the document. Handle the fact that the callback can't be called while aleady being called (no stackoverflow). Use the register pattern thus return the unregister function as a result
+ * @param {EventListener} callback 
+ * @return {()=>{}} The unregister function
+ */
+ const registerDomNodeInserted = (callback) => {
+    let nodeChangeInProgress = false
+
+    /** @type{EventListener} */
+    const onNodeChanged = (e) => {
+        if (!nodeChangeInProgress) {
+            nodeChangeInProgress = true
+            callback(e)
+            nodeChangeInProgress = false
+        }
+
+    }
+    document.documentElement.addEventListener('DOMNodeInserted', onNodeChanged, false);
+    onNodeChanged()
+    return () => {
+        document.documentElement.removeEventListener('DOMNodeInserted', onNodeChanged, false);
+    }
+}
---
+++ src/nightcafe/nightcafe-add-hot-new-buttons.js
@@ -0,0 +1,124 @@
+// ==UserScript==
+// @name        nightcafe-add-hot-new-buttons
+// @namespace   https://github.com/gissehel/userscripts
+// @match       https://creator.nightcafe.studio/*
+// @grant       none
+// @version     1.0.0
+// @author      none
+// @description Add keys to nightcafe.studio. Alt+s : Like/Unlike ; Alt+f : Shade or Unshade the liked images
+// ==/UserScript==
+
+/**
+ * Create a new element, and add some properties to it
+ * 
+ * @param {string} name The name of the element to create
+ * @param {object} params The parameters to tweek the new element
+ * @param {object.<string, string>} params.attributes The propeties of the new element
+ * @param {string} params.text The textContent of the new element
+ * @param {HTMLElement[]} params.children The children of the new element
+ * @param {HTMLElement} params.parent The parent of the new element
+ * @param {string[]} params.classnames The classnames of the new element
+ * @param {string} params.id The classnames of the new element
+ * @param {HTMLElement} params.prevSibling The previous sibling of the new element (to insert after)
+ * @param {HTMLElement} params.nextSibling The next sibling of the new element (to insert before)
+ * @param {(element:HTMLElement)=>{}} params.onCreated called when the element is fully created
+ */
+const createElementExtended = (name, params) => {
+    /** @type{HTMLElement} */
+    const element = document.createElement(name)
+    if (!params) {
+        params = {}
+    }
+    const { attributes, text, children, parent, classnames, id, prevSibling, nextSibling, onCreated } = params
+    if (attributes) {
+        for (let attributeName in attributes) {
+            element.setAttribute(attributeName, attributes[attributeName])
+        }
+    }
+    if (text) {
+        element.textContent = text;
+    }
+    if (children) {
+        for (let child of children) {
+            element.appendChild(child)
+        }
+    }
+    if (parent) {
+        parent.appendChild(element)
+    }
+    if (classnames) {
+        for (let classname of classnames) {
+            element.classList.add(classname)
+        }
+    }
+    if (id) {
+        element.id = id
+    }
+    if (prevSibling) {
+        prevSibling.parentElement.insertBefore(element, prevSibling.nextSibling)
+    }
+    if (nextSibling) {
+        nextSibling.parentElement.insertBefore(element, nextSibling)
+    }
+    if (onCreated) {
+        onCreated(element)
+    }
+    return element
+}
+
+/**
+ * Add a DOMNodeInserted on the document. Handle the fact that the callback can't be called while aleady being called (no stackoverflow). Use the register pattern thus return the unregister function as a result
+ * @param {EventListener} callback 
+ * @return {()=>{}} The unregister function
+ */
+const registerDomNodeInserted = (callback) => {
+    let nodeChangeInProgress = false
+
+    /** @type{EventListener} */
+    const onNodeChanged = (e) => {
+        if (!nodeChangeInProgress) {
+            nodeChangeInProgress = true
+            callback(e)
+            nodeChangeInProgress = false
+        }
+
+    }
+    document.documentElement.addEventListener('DOMNodeInserted', onNodeChanged, false);
+    onNodeChanged()
+    return () => {
+        document.documentElement.removeEventListener('DOMNodeInserted', onNodeChanged, false);
+    }
+}
+
+const explorerButtonSet = new Set()
+
+const onNodeChanged = (() => {
+    const explorerButtons = [...document.querySelectorAll('[data-testid=ExploreBtn]')]
+    for (let explorerButton of explorerButtons) {
+        if (!explorerButtonSet.has(explorerButton)) {
+            const liExplorer = explorerButton.parentElement
+            createElementExtended('li', {
+                children: [
+                    createElementExtended('a', {
+                        attributes: { 'href': '/explore?q=hottest' },
+                        text: 'Hot',
+                    })
+                ],
+                prevSibling: liExplorer
+            })
+            createElementExtended('li', {
+                children: [
+                    createElementExtended('a', {
+                        attributes: { 'href': '/explore?q=new' },
+                        text: 'New',
+                    })
+                ],
+                prevSibling: liExplorer
+            })
+
+            explorerButtonSet.add(explorerButton)
+        }
+    }
+})
+
+registerDomNodeInserted(onNodeChanged)
