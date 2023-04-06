08cdea15120ec1659f8820f935ec4cff261ae8ed
gissehel <gissehel@users.noreply.github.com>
2023-04-06 18:04:11 +0200

    Add copyTextToClipboard and a generic structure for userscripts

---
+++ snippet/userscript-shell.js
@@ -0,0 +1,8 @@
+(() => {
+    const script_name = GM_info.script.name
+    const script_version = GM_info.script.version
+    const script_id = `${script_name} ${script_version}`
+    console.log(`Begin - ${script_id}`)
+
+    console.log(`End - ${script_id}`)
+})()
