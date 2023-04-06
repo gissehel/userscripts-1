b41c31159b0437bcceb50a78137fce60dea7975d
gissehel <gissehel@users.noreply.github.com>
2023-04-06 09:56:36 +0200

    Add registerDomNodeInsertedUnique function

--- snippet/registerDomNodeInserted.js
+++ snippet/registerDomNodeInserted.js
@@ -1,5 +1,7 @@
 /**
- * Add a DOMNodeInserted on the document. Handle the fact that the callback can't be called while aleady being called (no stackoverflow). Use the register pattern thus return the unregister function as a result
+ * Add a DOMNodeInserted on the document. 
+ * Handle the fact that the callback can't be called while aleady being called (no stackoverflow). 
+ * Use the register pattern thus return the unregister function as a result
  * @param {EventListener} callback 
  * @return {()=>{}} The unregister function
  */
@@ -20,4 +22,27 @@
     return () => {
         document.documentElement.removeEventListener('DOMNodeInserted', onNodeChanged, false);
     }
+}
+
+/**
+ * Add a DOMNodeInserted on the document. 
+ * Handle the fact that the callback can't be called while aleady being called (no stackoverflow). 
+ * Use the register pattern thus return the unregister function as a result
+ * 
+ * Ensure that when an element matching the query elementProvider, the callback is called with the element 
+ * exactly once for each element
+ * @param {()=>[HTMLElement]} elementProvider 
+ * @param {(element: HTMLElement)=>{}} callback 
+ */
+const registerDomNodeInsertedUnique = (elementProvider, callback) => {
+    const domNodesHandled = new Set()
+    
+    return registerDomNodeInserted(()=>{
+        for (let element of elementProvider()) {
+            if (! domNodesHandled.has(element)) {
+                domNodesHandled.add(element)
+                callback(element)
+            }
+        }
+    })
 }
