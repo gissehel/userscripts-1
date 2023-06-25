289b3a91cd504f13dae058d23786c30e3381c059
gissehel <public-devgit-common@gissehel.org>
2023-06-25 14:18:44 +0200

    nullschool-link-to-webgeo : Use snippets

--- src/maps/nullschool-link-to-webgeo.user.js
+++ src/maps/nullschool-link-to-webgeo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nullschool-link-to-webgeo
 // @namespace    https://github.com/gissehel/userscripts
-// @version      1.1.0
+// @version      1.2.0
 // @description  nullschool-link-to-webgeo
 // @author       gissehel
 // @homepage     https://github.com/gissehel/userscripts
@@ -18,46 +18,182 @@
     const script_id = `${script_name} ${script_version}`
     console.log(`Begin - ${script_id}`)
 
-    let allowChanges = true;
-    let el = document.documentElement;
-    const link = document.createElement('a');
-    link.setAttribute('href', '#');
-    link.classList.add('nav-link');
-    const title = document.createElement('h1');
-    title.appendChild(link);
-    const button = document.createElement('button');
-    button.setAttribute('class', 'card no-touch-tt')
-    button.setAttribute('data-name', 'webgeo')
-    button.setAttribute('aria-controls', 'menu')
-    button.setAttribute('aria-labelledby', 'webgeo webgeo-tt')
-    button.setAttribute('data-tooltip', 'webgeo-tt')
-    button.setAttribute('title', 'Go to webgeo')
-    button.setAttribute('aria-expanded', 'true')
-    link.appendChild(button);
-    const spanTitle = document.createElement('span');
-    spanTitle.textContent = 'WebGeo';
-    button.appendChild(spanTitle);
-
-    const realLink = document.createElement('a');
-    realLink.setAttribute('href', '#');
-    realLink.setAttribute('target', '_blank');
-
-    const ondomchanged = () => {
-        if (allowChanges) {
-            allowChanges = false;
-            const titleBase = document.querySelector('h1');
-            if (titleBase) {
-                const parent = titleBase.parentElement;
-                parent.append(title);
-            } else {
-                allowChanges = true;
+    /**
+     * Wrap addEventListener and removeEventListener using a pattern where the unregister function is returned
+     * @param {EventTarget} eventTarget The object on which to register the event
+     * @param {string} eventType The event type
+     * @param {EventListenerOrEventListenerObject} callback The callback to call when the event is triggered
+     * @param {boolean|AddEventListenerOptions=} options The options to pass to addEventListener
+     */
+    const registerEventListener = (eventTarget, eventType, callback, options) => {
+        if (eventTarget.addEventListener) {
+            eventTarget.addEventListener(eventType, callback, options);
+        }
+        return () => {
+            if (eventTarget.removeEventListener) {
+                eventTarget.removeEventListener(eventType, callback, options);
             }
         }
-    };
+    }
 
-    const onGoToWebgeo = (e) => {
+    /**
+     * Add a DOMNodeInserted on the document. 
+     * Handle the fact that the callback can't be called while aleady being called (no stackoverflow). 
+     * Use the register pattern thus return the unregister function as a result
+     * @param {EventListener} callback 
+     * @return {()=>{}} The unregister function
+     */
+    const registerDomNodeInserted = (callback) => {
+        let nodeChangeInProgress = false
+
+        /** @type{EventListener} */
+        const onNodeChanged = (e) => {
+            if (!nodeChangeInProgress) {
+                nodeChangeInProgress = true
+                callback(e)
+                nodeChangeInProgress = false
+            }
+
+        }
+        document.documentElement.addEventListener('DOMNodeInserted', onNodeChanged, false);
+        onNodeChanged()
+        return () => {
+            document.documentElement.removeEventListener('DOMNodeInserted', onNodeChanged, false);
+        }
+    }
+
+    /**
+     * Add a DOMNodeInserted on the document. 
+     * Handle the fact that the callback can't be called while aleady being called (no stackoverflow). 
+     * Use the register pattern thus return the unregister function as a result
+     * 
+     * Ensure that when an element matching the query elementProvider, the callback is called with the element 
+     * exactly once for each element
+     * @param {()=>[HTMLElement]} elementProvider 
+     * @param {(element: HTMLElement)=>{}} callback 
+     */
+    const registerDomNodeInsertedUnique = (elementProvider, callback) => {
+        const domNodesHandled = new Set()
+
+        return registerDomNodeInserted(() => {
+            for (let element of elementProvider()) {
+                if (!domNodesHandled.has(element)) {
+                    domNodesHandled.add(element)
+                    const result = callback(element)
+                    if (result === false) {
+                        domNodesHandled.delete(element)
+                    }
+                }
+            }
+        })
+    }
+
+    /**
+     * Create a new element, and add some properties to it
+     * 
+     * @param {string} name The name of the element to create
+     * @param {object} params The parameters to tweek the new element
+     * @param {object.<string, string>} params.attributes The propeties of the new element
+     * @param {string} params.text The textContent of the new element
+     * @param {HTMLElement[]} params.children The children of the new element
+     * @param {HTMLElement} params.parent The parent of the new element
+     * @param {string[]} params.classnames The classnames of the new element
+     * @param {string} params.id The classnames of the new element
+     * @param {HTMLElement} params.prevSibling The previous sibling of the new element (to insert after)
+     * @param {HTMLElement} params.nextSibling The next sibling of the new element (to insert before)
+     * @param {(element:HTMLElement)=>{}} params.onCreated called when the element is fully created
+     * @returns {HTMLElement} The created element
+     */
+    const createElementExtended = (name, params) => {
+        /** @type{HTMLElement} */
+        const element = document.createElement(name)
+        if (!params) {
+            params = {}
+        }
+        const { attributes, text, children, parent, classnames, id, prevSibling, nextSibling, onCreated } = params
+        if (attributes) {
+            for (let attributeName in attributes) {
+                element.setAttribute(attributeName, attributes[attributeName])
+            }
+        }
+        if (text) {
+            element.textContent = text;
+        }
+        if (children) {
+            for (let child of children) {
+                element.appendChild(child)
+            }
+        }
+        if (parent) {
+            parent.appendChild(element)
+        }
+        if (classnames) {
+            for (let classname of classnames) {
+                element.classList.add(classname)
+            }
+        }
+        if (id) {
+            element.id = id
+        }
+        if (prevSibling) {
+            prevSibling.parentElement.insertBefore(element, prevSibling.nextSibling)
+        }
+        if (nextSibling) {
+            nextSibling.parentElement.insertBefore(element, nextSibling)
+        }
+        if (onCreated) {
+            onCreated(element)
+        }
+        return element
+    }
+
+    const link = createElementExtended('a', {
+        attributes: {
+            href: '#'
+        },
+        classnames: ['nav-link'],
+        children: [
+            createElementExtended('h1', {
+                parent: link,
+            }),
+            createElementExtended('button', {
+                attributes: {
+                    class: 'card no-touch-tt',
+                    'data-name': 'webgeo',
+                    'aria-controls': 'menu',
+                    'aria-labelledby': 'webgeo webgeo-tt',
+                    'data-tooltip': 'webgeo-tt',
+                    title: 'Go to webgeo',
+                    'aria-expanded': 'true',
+                },
+                parent: link,
+                children: [
+                    createElementExtended('span', {
+                        text: 'WebGeo',
+                    }),
+                ],
+            }),
+        ],            
+    })
+
+    const realLink = createElementExtended('a', {
+        attributes: {
+            href: '#',
+            target: '_blank',
+        },
+    })
+
+    registerDomNodeInsertedUnique(() => document.querySelector('h1'), (titleBase) => {
+        const parent = titleBase.parentElement;
+        if (parent) {
+            parent.append(link);
+            return true
+        }
+        return false
+    })
+
+    registerEventListener(link, 'click', (e) => {
         e.preventDefault();
-
         const locs = location.hash.split('/').filter(x => x.startsWith('loc='));
         if (locs.length > 0) {
             const loc = locs[0].substring('loc='.length)
@@ -67,11 +203,7 @@
             realLink.click();
         }
         return true;
-    }
-
-    el.addEventListener('DOMNodeInserted', ondomchanged, false);
-    link.addEventListener('click', onGoToWebgeo, false);
-    ondomchanged();
+    })
 
     console.log(`End - ${script_id}`)
 })()
