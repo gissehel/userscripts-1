19ad6ad89eef0fe8e6b81f7c4be7ae0535da4893
gissehel <public-devgit-common@gissehel.org>
2023-06-25 15:46:14 +0200

    nightcafe-add-keys : Rollback

--- src/nightcafe/nightcafe-add-keys.user.js
+++ src/nightcafe/nightcafe-add-keys.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         nightcafe-add-keys
 // @namespace    https://github.com/gissehel/userscripts
-// @version      1.2.1
+// @version      1.1.0
 // @description Add keys to nightcafe.studio. Alt+s : Like/Unlike ; Alt+f : Shade or Unshade the liked images
 // @author       gissehel
 // @homepage     https://github.com/gissehel/userscripts
 // @supportURL   https://github.com/gissehel/userscripts/issues
-// @match        https://creator.nightcafe.studio/*
+// @match       https://creator.nightcafe.studio/*
 // @icon         https://creator.nightcafe.studio/favicon-32x32.png
 // @grant        none
 // ==/UserScript==
@@ -124,9 +124,7 @@
     })();
 
     /**
-     * Add a DOMNodeInserted on the document. 
-     * Handle the fact that the callback can't be called while aleady being called (no stackoverflow). 
-     * Use the register pattern thus return the unregister function as a result
+     * Add a DOMNodeInserted on the document. Handle the fact that the callback can't be called while aleady being called (no stackoverflow). Use the register pattern thus return the unregister function as a result
      * @param {EventListener} callback 
      * @return {()=>{}} The unregister function
      */
@@ -149,60 +147,6 @@
         }
     }
 
-    /**
-     * Add a DOMNodeInserted on the document. 
-     * Handle the fact that the callback can't be called while aleady being called (no stackoverflow). 
-     * Use the register pattern thus return the unregister function as a result
-     * 
-     * Ensure that when an element matching the query elementProvider, the callback is called with the element 
-     * exactly once for each element
-     * @param {()=>[HTMLElement]} elementProvider 
-     * @param {(element: HTMLElement)=>{}} callback 
-     */
-    const registerDomNodeInsertedUnique = (elementProvider, callback) => {
-        const domNodesHandled = new Set()
-
-        return registerDomNodeInserted(() => {
-            for (let element of elementProvider()) {
-                if (!domNodesHandled.has(element)) {
-                    domNodesHandled.add(element)
-                    const result = callback(element)
-                    if (result === false) {
-                        domNodesHandled.delete(element)
-                    }
-                }
-            }
-        })
-    }
-
-    const parentsElement = (element, depth) => {
-        let current_element = element
-        for (let index = 0; index < depth; index++) {
-            current_element = current_element?.parentElement
-        }
-        return current_element
-    }
-
-    registerDomNodeInsertedUnique(() => [...document.querySelectorAll('.renderIfVisible')], (image) => {
-        const parentElementClassList = image.parentElement.classList
-        const liked = [...image.querySelectorAll('path[d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"]')]
-        if (liked.length > 0) {
-            parentElementClassList.add('isLiked')
-            parentElementClassList.remove('isNotLiked')
-        } else {
-            parentElementClassList.add('isNotLiked')
-            parentElementClassList.remove('isLiked')
-        }
-    })
-
-    for (let [pattern, depth] of [["[href*='https://reddit.com/r/nightcafe']", 8], ["[style*='lounge-bg.jpg']", 3], ["[href*='/pricing#pro']", 2]]) {
-        registerDomNodeInsertedUnique(() => document.querySelectorAll(pattern), (spamZone) => {
-            const zoneToSuppress = parentsElement(spamZone, depth)
-            if (zoneToSuppress && !zoneToSuppress.classList.contains('hidden-element')) {
-                zoneToSuppress.classList.add('hidden-element')
-            }
-        })
-    }
 
     const likeOrUnlike = (e) => {
         const likeOrUnlikeButtons = [...document.querySelectorAll('[data-testid="JobPopup"] button[title=Unlike]'), ...document.querySelectorAll('[data-testid="JobPopup"] button[title=Like]')]
@@ -213,6 +157,38 @@
         e.preventDefault()
     }
 
+    const parentsElement = (element, depth) => {
+        let current_element = element
+        for (let index = 0; index < depth; index++) {
+            current_element = current_element?.parentElement
+        }
+        return current_element
+    }
+
+    const onDomChanged = () => {
+        const images = [...document.querySelectorAll('.renderIfVisible')]
+        for (let image of images) {
+            const parentElementClassList = image.parentElement.classList
+            const liked = [...image.querySelectorAll('path[d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"]')]
+            if (liked.length > 0) {
+                parentElementClassList.add('isLiked')
+                parentElementClassList.remove('isNotLiked')
+            } else {
+                parentElementClassList.add('isNotLiked')
+                parentElementClassList.remove('isLiked')
+            }
+        }
+        for (let [pattern, depth] of [["[href*='https://reddit.com/r/nightcafe']", 8], ["[style*='lounge-bg.jpg']", 3], ["[href*='/pricing#pro']", 2]]) {
+            const spamZones = [...document.querySelectorAll(pattern)]
+            for (let spamZone of spamZones) {
+                const zoneToSuppress = parentsElement(spamZone, depth)
+                if (zoneToSuppress && !zoneToSuppress.classList.contains('hidden-element')) {
+                    zoneToSuppress.classList.add('hidden-element')
+                }
+            }
+        }
+    }
+
     const shadeOrUnshadeLiked = (e) => {
         if (document.body.classList.contains('shadeLiked')) {
             document.body.classList.remove('shadeLiked')
@@ -221,6 +197,8 @@
         }
         e.preventDefault()
     }
+
+    registerDomNodeInserted(onDomChanged)
 
     const unregisterLikeOrUnlike = registerKeyStruct({ key: 's', altKey: true }, likeOrUnlike);
     const unregisterShadeOrUnshadeLiked = registerKeyStruct({ key: 'f', altKey: true }, shadeOrUnshadeLiked);
