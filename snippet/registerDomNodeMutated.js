5defe3fd4eb1255ce3b2c4aa4235aedae072eb3a
gissehel <public-devgit-common@gissehel.org>
2023-08-12 15:27:35 +0200

    Add youtube-pip

---
+++ snippet/registerDomNodeMutated.js
@@ -0,0 +1,50 @@
+/**
+ * Call the callback when the document change
+ * Handle the fact that the callback can't be called while aleady being called (no stackoverflow). 
+ * Use the register pattern thus return the unregister function as a result
+ * @param {()=>()} callback 
+ * @return {()=>{}} The unregister function
+ */
+const registerDomNodeMutated = (callback) => {
+    let callbackInProgress = false
+
+    const action = () => {
+        if (!callbackInProgress) {
+            callbackInProgress = true
+            callback()
+            callbackInProgress = false
+        }
+    }
+
+    const mutationObserver = new MutationObserver((mutationsList, observer) => { action() });
+    action()
+    mutationObserver.observe(document.documentElement, { childList: true, subtree: true });
+
+    return () => mutationObserver.disconnect()
+}
+
+/**
+ * Call the callback once per element provided by the elementProvider when the document change
+ * Handle the fact that the callback can't be called while aleady being called (no stackoverflow). 
+ * Use the register pattern thus return the unregister function as a result
+ * 
+ * Ensure that when an element matching the query elementProvider, the callback is called with the element 
+ * exactly once for each element
+ * @param {()=>[HTMLElement]} elementProvider 
+ * @param {(element: HTMLElement)=>{}} callback 
+ */
+const registerDomNodeMutatedUnique = (elementProvider, callback) => {
+    const domNodesHandled = new Set()
+
+    return registerDomNodeMutated(() => {
+        for (let element of elementProvider()) {
+            if (!domNodesHandled.has(element)) {
+                domNodesHandled.add(element)
+                const result = callback(element)
+                if (result === false) {
+                    domNodesHandled.delete(element)
+                }
+            }
+        }
+    })
+}
