8c45ad7e9c25737d8b657c9453f75a8af5e1bfcc
gissehel <public-devgit-common@gissehel.org>
2023-09-03 11:45:45 +0200

    Add notion-seticon

---
+++ src/notion/notion-seticon.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @version      1.0.0
+// @description  Fix notion "set icon" that randomly don't set page icon to tab icon
+// ==/UserScript==
+
+// @import{registerDomNodeInsertedUnique}
+// @import{getSubElements}
+
+registerDomNodeInsertedUnique(() => getElements('.pseudoSelection>.notion-record-icon'), (icon_container) => {
+    const hrefs = getSubElements(icon_container, 'img')
+        .map((image)=>image.getAttribute('src'))
+        .filter((href)=>href.startsWith('https://'))
+
+    if (hrefs.length === 0) {
+        return false
+    }
+    const href = hrefs[hrefs.length - 1]
+
+    const xicons = getElements('head > [type="image/x-icon"]')
+    if (xicons.length === 0) {
+        return false
+    }
+    const xicon = xicons[xicons.length - 1]
+
+    xicon.setAttribute('href', href)
+    
+    return true
+})
