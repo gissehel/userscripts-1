f88e69c21939be52a4516ab64b38facf47111253
gissehel <public-devgit-common@gissehel.org>
2024-01-04 13:17:08 +0100

    twittervideodownloader : Update to new version of the site...

--- src/twitter/twittervideodownloader-easy.user.js
+++ src/twitter/twittervideodownloader-easy.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version      3.0.0
+// @version      3.0.1
 // @description  twittervideodownloader-easy
 // @match        http://twittervideodownloader.com/*
 // @match        https://twittervideodownloader.com/*
@@ -13,7 +13,7 @@ if (input) {
 
 document.body.onkeyup = (e) => {
     if (e.code == 'KeyV' && e.ctrlKey) {
-        const button = document.querySelectorAll('input[type=submit]')[0];
+        const button = document.querySelectorAll('button[type=submit]')[0];
         if (button) {
             button.click();
         }
@@ -33,15 +33,16 @@ const download = async (url, name) => {
     window.URL.revokeObjectURL(urlContent);
 };
 
-const domSizes = [...document.querySelectorAll('.small-6 p.float-left')];
+const domSizes = [...document.querySelectorAll('.btn.fw-bold.tw-btn.btn-sm')];
 if (domSizes.length) {
     const maxDomSize = domSizes.map((domSize) => {
-        let [width, height] = domSize.outerText.split(' : ')[0].split('x').map(z => 1 * z);
+        let [width, height] = domSize.outerText.split(': ')[0].split(' ')[1].split('x').map(z => 1 * z);
         return { domSize, area: width * height };
     }).sort((result1, result2) => result2.area - result1.area)[0].domSize;
-    const link = maxDomSize.parentElement.parentElement.getElementsByTagName('a')[0];
+    const link = maxDomSize
     link.style.backgroundColor = 'red';
     const url = link.getAttribute('href');
     const filename = url.split('/').slice(-1)[0].split('?')[0];
-    download(url, filename);
+    // download doesn't work anymore as cors forbids automatic download
+    // download(url, filename);
 }
